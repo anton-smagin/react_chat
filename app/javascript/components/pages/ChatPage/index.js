e96f97a4e7d215032e035a1ea0c6328846f74586
Anton Smagin <smagiman@gmail.com>
2017-12-26 15:08:47 +0300

    add react chat components

--- app/javascript/components/pages/ChatPage/index.js
+++ app/javascript/components/pages/ChatPage/index.js
@@ -1,1 +1,7 @@
-export default () => { return 'ChatPage'}
+import React from 'react'
+import { ChatList, ChatRoom } from 'components'
+export default () => {
+  return (
+    <div> <ChatList /> <ChatRoom /> </div>
+    )
+}
